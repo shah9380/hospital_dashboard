1f7a7c9b9142c4998e5012590cfb7842f832a682
shah9380 <130676464+shah9380@users.noreply.github.com>
2024-04-08 00:29:43 +0530

    changes made

--- src/components/AddNewAdmin.jsx
+++ src/components/AddNewAdmin.jsx
@@ -11,7 +11,7 @@ const AddNewAdmin = () => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
-  const [nic, setNic] = useState("");
+  const [aadharNumber, setAadharNumber] = useState("");
   const [dob, setDob] = useState("");
   const [gender, setGender] = useState("");
   const [password, setPassword] = useState("");
@@ -24,7 +24,7 @@ const AddNewAdmin = () => {
       await axios
         .post(
           "https://hotel-management-mern-1.onrender.com/api/v1/user/admin/addnew",
-          { firstName, lastName, email, phone, nic, dob, gender, password },
+          { firstName, lastName, email, phone, aadharNumber, dob, gender, password },
           {
             withCredentials: true,
             headers: { "Content-Type": "application/json" },
@@ -38,7 +38,7 @@ const AddNewAdmin = () => {
           setLastName("");
           setEmail("");
           setPhone("");
-          setNic("");
+          setAadharNumber("");
           setDob("");
           setGender("");
           setPassword("");
@@ -89,9 +89,9 @@ const AddNewAdmin = () => {
           <div>
             <input
               type="number"
-              placeholder="NIC"
-              value={nic}
-              onChange={(e) => setNic(e.target.value)}
+              placeholder="Aadhar"
+              value={aadharNumber}
+              onChange={(e) => setAadharNumber(e.target.value)}
             />
             <input
               type={"date"}
--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -52,7 +52,7 @@ const Doctors = () => {
                     Department: <span>{element.doctorDepartment}</span>
                   </p>
                   <p>
-                    NIC: <span>{element.nic}</span>
+                    Aadhar: <span>{element.aadharNumber}</span>
                   </p>
                   <p>
                     Gender: <span>{element.gender}</span>
